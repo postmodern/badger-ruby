604637ffc35bb1f23d58b0cef5020a6f6edb0037
postmodern <postmodern.mod3@gmail.com>
2010-05-30 01:49:23 -0700

    Added more methods to Services::Sys.

--- lib/badger/services/sys.rb
+++ lib/badger/services/sys.rb
@@ -12,8 +12,40 @@ module Badger
         call(:chdir,path)
       end
 
-      def getcwd
+      def get_hostname
+        call(:gethostname)
+      end
+
+      def get_domainname
+        call(:getdomainname)
+      end
+
+      def get_cwd
         call(:getcwd)
+      end
+
+      def get_pid
+        call(:getpid)
+      end
+
+      def get_uid
+        call(:getuid)
+      end
+
+      def get_euid
+        call(:geteuid)
+      end
+
+      def get_gid
+        call(:getgid)
+      end
+
+      def get_egid
+        call(:getegid)
+      end
+
+      def popen(command,&block)
+        call(:popen,command.to_s,&block)
       end
 
     end
