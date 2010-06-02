607e031d8597c920c172c1f5792cd85541cfcfcd
postmodern <postmodern.mod3@gmail.com>
2010-06-02 00:02:16 -0700

    Added a RemoteFile to proxy read/write/seek calls to the FS Service.

---
+++ lib/badger/remote_file.rb
@@ -0,0 +1,32 @@
+module Badger
+  class RemoteFile < IO
+
+    SEEK_NAMES = {
+      SEEK_SET => :set,
+      SEEK_CUR = > :current,
+      SEEK_END => :end
+    }
+
+    def initialize(fs,fd)
+      @fs = fs
+      @fd = fd
+    end
+
+    def sysseek(offset,whence=SEEK_SET)
+      @fs.call(:seek,@fd,offset,SEEK_NAMES[whence])
+    end
+
+    def sysread(length)
+      @fs.call(:read,@fd,length)
+    end
+
+    def syswrite(data)
+      @fs.call(:write,@fd,data)
+    end
+
+    def inspect
+      "#<#{self.class}: #{@fs.client.uri}: @fd=#{@fd}>"
+    end
+
+  end
+end
